4dde87e5078755e8351f49f973a0bf2c6c6b5dd5
leo <nghiemnguyenvu@lgcns.com>
2024-10-14 12:52:10 +0900

    Updates

--- assets/LifeCare-3FmsXdCq.js
+++ assets/LifeCare-3FmsXdCq.js
@@ -1,4 +1,4 @@
-import{S as ae,r as se,M as ie,_ as ne}from"./vue3-grid-layout-C2IJjFBD.js";import{d as re,r as h,i as oe,j as ce,k as ue,a as w,c as M,b as n,w as c,u as i,g as L,n as E,l as S,m as pe,v as de,p as I,F as B,h as C,q as me,s as j,t as D,T as fe,x as k,I as q,S as F,y as G,z as ge,B as T,D as he,A as U,E as ve,G as ke}from"./index-DQp7ueh_.js";const _e=`<!DOCTYPE html>
+import{S as ae,r as se,M as ie,_ as ne}from"./vue3-grid-layout-BHobfYx5.js";import{d as re,r as h,i as oe,j as ce,k as ue,a as w,c as M,b as n,w as c,u as i,g as L,n as E,l as S,m as pe,v as de,p as I,F as B,h as C,q as me,s as j,t as D,T as fe,x as k,I as q,S as F,y as G,z as ge,B as T,D as he,A as U,E as ve,G as ke}from"./index-XdSKaBPA.js";const _e=`<!DOCTYPE html>
 <html lang="ko" style="height: 100%;">
   <head>
     <meta charset="utf-8" />
