914a90e7094386efb8e9c3026591ddd56ff57907
leo <nghiemnguyenvu@lgcns.com>
2024-10-14 12:54:58 +0900

    deploy 1.0.6

--- src/views/EventGuide.vue
+++ src/views/EventGuide.vue
@@ -1,8 +1,9 @@
 <template>
-  <div>NGHIEM</div>
+  <iframe src="https://ux.lglifecare.com/uxtech/area/guide_event.html" frameborder="0" class="w-full h-full"></iframe>
 </template>
 
 <script setup lang="ts">
+
 </script>
 
 <style scoped></style>
